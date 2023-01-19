f5cc0b320d55268c67015254e14d8b57ce4cb276
hiroki02 <hiroki.yamaguchi@aisysnet.com>
2023-01-19 09:47:31 +0900

    おすすめコンテンツ追加、入れ替え作業も含む

--- src/components/pages/RecommendedContentsComponent.vue
+++ src/components/pages/RecommendedContentsComponent.vue
@@ -4,10 +4,10 @@
     <v-divider class="mx-4" inset vertical></v-divider>
 
     <v-spacer></v-spacer>
-    <v-data-table :headers="headers" :items="contentsList" sort-by="title" class="elevation-1">
+    <v-data-table :headers="headers" :items="contentsList" class="elevation-1">
       <!-- アイキャッチ画像 -->
       <template v-slot:[`item.image`]="{ item }">
-        <v-img max-height="150" max-width="250" :src="item.image"></v-img>
+        <v-img width="120px" height="120px" v-bind:src="item.image"></v-img>
       </template>
       <!-- サブタイトル -->
       <template v-slot:[`item.subtitle`]="{ item }">
@@ -33,6 +33,18 @@
           {{ "削除済" }}
         </div>
       </template>
+      <template v-slot:[`item.order`]="{ item }">
+        <div>
+          <v-col cols="2" class="d-flex align-center">
+            <v-icon :disabled="!changeOrderFlag" @click="switchListItem(item, 1, 'up')"
+              >mdi-chevron-up</v-icon
+            >
+            <v-icon :disabled="!changeOrderFlag" @click="switchListItem(item, 1, 'down')"
+              >mdi-chevron-down</v-icon
+            >
+          </v-col>
+        </div>
+      </template>
 
       <!-- 編集・削除アイコン -->
       <template v-slot:[`item.updDel`]="{ item }">
@@ -46,10 +58,20 @@
     </v-data-table>
 
     <v-row class="pa-4">
-      <v-col cols="8">
+      <v-col cols="10">
         <v-btn color="primary" @click="editNewItem">
           <v-icon left> mdi-plus </v-icon>
           新規登録
+        </v-btn>
+      </v-col>
+      <v-col cols="2">
+        <v-btn color="grey" @click="changeOrder" v-if="!changeOrderFlag">
+          <v-icon left> mdi-arrow-up-down </v-icon>
+          表示順を変更する
+        </v-btn>
+        <v-btn color="grey" @click="orderDecision" v-else>
+          <v-icon left> mdi-content-save-check </v-icon>
+          表示順を確定する
         </v-btn>
       </v-col>
     </v-row>
@@ -66,8 +88,9 @@
             <v-row>
               <v-col>
                 <v-file-input
+                  type="file"
                   v-model="editedItem.image"
-                  accept=".png"
+                  accept="image/png, image/jpeg"
                   show-size
                   counter
                   outlined
@@ -111,38 +134,47 @@ export default {
     PageHeadComponent,
   },
   data: () => ({
+    pageHeadTitle: "おすすめコンテンツ",
     // 検索用メニュー
     searchUserMenu: false,
     valid: true,
     userChoice: 2,
 
-    pageHeadTitle: "おすすめコンテンツ",
     dialogEdit: false,
 
     headers: [
-      { text: "アイキャッチ画像", value: "image", width: "30%", align: "start" },
+      { text: "アイキャッチ画像", value: "image", width: "15%", align: "start" },
       { text: "サブタイトル", value: "subtitle", width: "20%", align: "start" },
       { text: "タイトル", value: "title", width: "20%", align: "start" },
-      { text: "URL", value: "url", width: "20%", align: "start" },
+      { text: "URL", value: "url", width: "25%", align: "start" },
       { text: "編集・削除", value: "updDel", sortable: false, width: "10%", align: "start" },
+      {
+        text: "表示順序",
+        value: "order",
+        sortable: false,
+        width: "10%",
+        align: "start",
+      },
     ],
+
     contentsList: [],
     editedIndex: -1,
-
     editedItem: {
       url: "",
       subtitle: "",
       title: "",
-      image: "",
+      image: null,
       status: false,
     },
     defaultItem: {
       url: "",
       subtitle: "",
       title: "",
-      image: "",
+      image: null,
       status: false,
     },
+    // 表示順変更フラグ
+    changeOrderFlag: false,
   }),
 
   computed: {
@@ -164,9 +196,10 @@ export default {
   methods: {
     initialize() {
       this.contentsList = [
+        // TODO:オブジェクト URLをimageに追加
         {
           image:
-            "https://s3.console.aws.amazon.com/s3/object/amplify-sdgsappmanagement-main-91300-deployment?region=us-east-2&prefix=basketball.jpg",
+            "https://amplify-sdgsappmanagement-main-91300-deployment.s3.us-east-2.amazonaws.com/basketball.jpg",
           subtitle: "エコプロ2022セブン＆愛グループ特設サイト",
           title: "環境モンスターをクイズでやっつけよう",
           url: "https://www.7andi.com/company/news/release/202210271100.html",
@@ -174,7 +207,7 @@ export default {
         },
         {
           image:
-            "https://s3.console.aws.amazon.com/s3/object/amplify-sdgsappmanagement-main-91300-deployment?region=us-east-2&prefix=basketball.jpg",
+            "https://amplify-sdgsappmanagement-main-91300-deployment.s3.us-east-2.amazonaws.com/volleyball.png",
           subtitle: "たいせつにつくる、たいせつに食べる",
           title: "賢者のレシピ",
           url: "https://www.7andi.com/company/news/release/202210271100.html",
@@ -215,18 +248,44 @@ export default {
     },
 
     save() {
-      console.log(this.editedItem.image);
       if (this.editedIndex > -1) {
         // 編集押下されたとき
         Object.assign(this.contentsList[this.editedIndex], this.editedItem);
       } else {
         // 新規登録が押下されたとき
+        // TODO: 選択された画像ファイルをS3の指定箇所に格納する→amplifyにいい感じの機能がありそうだから実装前に調べてみること
+        // s3に格納したファイルをオブジェクトURL形式で指定のDBへ格納→リストとして取り出し、画面に表示する
         this.contentsList.push(this.editedItem);
       }
       this.close();
     },
     editNewItem() {
       this.dialogEdit = true;
+    },
+
+    // 表示されてるデータの表示順を変更する
+    changeOrder() {
+      // ソートができる機能をテーブルに表示する→今のとこ、マウスで操作できる仕様にしようと思ってる
+
+      this.changeOrderFlag = true;
+    },
+    // 表示順を確定する
+    orderDecision() {
+      // TODO: 入れ替えられた順序で配列をテーブルに保存する処理
+      console.log(this.contentsList);
+      this.changeOrderFlag = false;
+    },
+    // 表示順序変更
+    switchListItem: function (item, i, type) {
+      let array = this.contentsList;
+      let index = array.indexOf(item);
+      array.splice(index, 1);
+      if (type == "up") {
+        array.splice(index - 1, 0, item);
+      }
+      if (type == "down") {
+        array.splice(index + 1, 0, item);
+      }
     },
   },
 };
